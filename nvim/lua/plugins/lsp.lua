91ee7ef12df187d6345da1affe4a38dac27956ef
bphkns <bphkns@gmail.com>
2024-01-29 01:04:59 +0530

    Fixed lazyvim

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      ensure_installed = {
+        "nxls",
+        "angularls",
+      },
+      servers = {
+        angularls = {
+          root_dir = require("lspconfig").util.root_pattern("angular.json", "project.json"),
+          filetypes = { "angular", "html", "typescript", "typescriptreact" },
+        },
+      },
+    },
+  },
+}
