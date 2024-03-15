88226f41c835a2a084774f3b3bf873314c6b05ba
bphkns <bphkns@gmail.com>
2024-03-15 20:52:43 +0530

    update

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.leap" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.editor.symbols-outline" },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,43 +1,10 @@
-local Util = require("lazyvim.util")
-
 return {
-  {
-    "telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
-    },
-    opts = {
-      defaults = {
-        preview = {
-          hide_on_startup = true,
-        },
-        layout_config = {
-          width = 0.8,
-          horizontal = {
-            preview_width = 0.9,
-          },
-        },
-        mappings = {
-          i = {
-            ["<C-a>"] = require("telescope.actions.layout").toggle_preview,
-          },
-        },
-      },
-    },
-    keys = {
-      { "<leader><space>", Util.telescope("files", { cwd = false }), desc = "Find Files (root dir)" },
-      { "<leader>fF", Util.telescope("files"), desc = "Find Files (cwd)" },
-      {
-        "<leader>ff",
-        function()
-          require("telescope.builtin").find_files({ hidden = true, no_ignore = true })
-        end,
-        desc = "Find Files (root dir, hidden files, no ignored)",
-      },
-    },
-  },
+  "nvim-telescope/telescope.nvim",
+  -- replace all Telescope keymaps with only one mapping
+  keys = function()
+    return {
+      { "<leader> ", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+      { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+    }
+  end,
 }
