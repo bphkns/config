032c12986f395cf7766154cb77b7a3631d40d425
Bikash Phukan <bikash@spotdraft.com>
2024-05-22 15:49:32 +0530

    a

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.editor.trouble-v3" },
+    { import = "lazyvim.plugins.extras.ui.treesitter-context" },
     { import = "lazyvim.plugins.extras.editor.refactoring" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.editor.navic" },
